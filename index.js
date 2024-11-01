d3fecbca12be0b60c132b5b96a456ecde8a7f6d4
Daniel <daniellim637@gmail.com>
2024-11-01 17:39:36 +0800

    added allowed cors origin

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import adminRoutes from './src/routes/adminRoutes.js'
+import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
+
+app.use(cors());
 app.use(bodyParser.json());
 
 
